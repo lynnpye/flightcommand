a8bc78121fd753bb77bc9d0426f900fb05ff9e54
Lynn Pye <github@pyehouse.com>
2021-08-15 22:52:48 -0500

    Fixed: gamerule doesn't reapply on restart and other issues

---
+++ src/main/java/com/pyehouse/mcmod/flightcommand/api/util/Tools.java
@@ -0,0 +1,2 @@
+package com.pyehouse.mcmod.flightcommand.api.util;public class Tools {
+}
--- src/main/java/com/pyehouse/mcmod/flightcommand/common/handler/CommonPlayerTickEventHandler.java
+++ src/main/java/com/pyehouse/mcmod/flightcommand/common/handler/CommonPlayerTickEventHandler.java
@@ -2,9 +2,12 @@ package com.pyehouse.mcmod.flightcommand.common.handler;
 
 import com.pyehouse.mcmod.flightcommand.api.capability.FlightCapability;
 import com.pyehouse.mcmod.flightcommand.api.capability.IFlightCapability;
+import com.pyehouse.mcmod.flightcommand.api.util.Tools;
 import com.pyehouse.mcmod.flightcommand.common.command.GameruleRegistry;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
@@ -13,6 +16,19 @@ import org.apache.logging.log4j.Logger;
 public class CapabilityPlayerTickEventHandler {
 
     private static final Logger LOGGER = LogManager.getLogger();
+
+    @SubscribeEvent
+    public static void onPlayerClone(PlayerEvent.Clone event) {
+        PlayerEntity player = event.getPlayer();
+        LOGGER.info("ZZZZZZZZZZZZZZZZZZZZZZZZZ event onPlayerClone");
+        LOGGER.info("gamerule to allow flight " + Tools.isWorldFlightOn(player));
+        IFlightCapability flightCap = player.getCapability(FlightCapability.CAPABILITY_FLIGHT).orElse(null);
+        IFlightCapability oldFlightCap = event.getOriginal().getCapability(FlightCapability.CAPABILITY_FLIGHT).orElse(null);
+        flightCap.setAllowedFlight(oldFlightCap.isAllowedFlight());
+
+        // not necessary to sync, both sides recloning?
+        //SyncEventHandler.syncPlayerData(player);
+    }
 
     @SubscribeEvent
     public static void playerTick(TickEvent.PlayerTickEvent event) {
@@ -32,17 +48,19 @@ public class CapabilityPlayerTickEventHandler {
             return; // no capability present for some reason
         }
 
-        if (flightCap.isWorldFlightEnabled() != GameruleRegistry.isEnabled(player.getCommandSenderWorld(), GameruleRegistry.doCreativeFlight)) {
+        boolean worldFlightOn = Tools.isWorldFlightOn(player);
+        if (flightCap.isWorldFlightEnabled() != worldFlightOn) {
             flightCap.setWorldFlightEnabled(!flightCap.isWorldFlightEnabled());
             flightCap.setShouldCheckFlight(true);
         }
 
         // now check capabilities and set abilities
-        if (!player.abilities.mayfly && (isWorldFlightOn(player) || !canRemoveFlightByGamemode(player) || flightCap.isAllowedFlight() || flightCap.isShouldCheckFlight())) {
+        if (!player.abilities.mayfly && (worldFlightOn || !Tools.canRemoveFlightByGamemode(player) || flightCap.isAllowedFlight() || flightCap.isShouldCheckFlight())) {
             // should be able to fly but can't, we can fix that
             player.abilities.mayfly = true;
+            flightCap.setShouldCheckFlight(false);
             player.onUpdateAbilities();
-        } else if (player.abilities.mayfly && !isWorldFlightOn(player) && canRemoveFlightByGamemode(player) && !flightCap.isAllowedFlight() && flightCap.isShouldCheckFlight()) {
+        } else if (player.abilities.mayfly && !worldFlightOn && Tools.canRemoveFlightByGamemode(player) && !flightCap.isAllowedFlight() && flightCap.isShouldCheckFlight()) {
             LOGGER.info("Disabling flight and possibly causing fall damage");
             player.abilities.mayfly = false; // hopefully anything else allowing flight will revert this
             if (player.abilities.flying) {
@@ -51,25 +69,5 @@ public class CapabilityPlayerTickEventHandler {
             player.onUpdateAbilities();
             flightCap.setShouldCheckFlight(false);
         }
-    }
-
-    private static boolean isWorldFlightOn(PlayerEntity player) {
-        try {
-            return player.getCommandSenderWorld().getGameRules().getBoolean(GameruleRegistry.doCreativeFlight);
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    private static boolean canRemoveFlightByGamemode(PlayerEntity player) {
-        return !(player.isSpectator() || player.isCreative());
-    }
-
-    @SubscribeEvent
-    public static void onPlayerClone(PlayerEvent.Clone event) {
-        PlayerEntity player = event.getPlayer();
-        IFlightCapability flightCap = player.getCapability(FlightCapability.CAPABILITY_FLIGHT).orElse(null);
-        IFlightCapability oldFlightCap = event.getOriginal().getCapability(FlightCapability.CAPABILITY_FLIGHT).orElse(null);
-        flightCap.setAllowedFlight(oldFlightCap.isAllowedFlight());
     }
 }
